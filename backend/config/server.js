b803c89277aa72cc3ba695ad05117b910f323079
darrendb <darrendb@gmail.com>
2021-08-06 18:17:39 -0400

    initial commit from strapi-starter

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '38a44c0055f32dd47b6d4842220699a4'),
+    },
+  },
+});
