defbb4ab7d04e39a1e804100ce85e5cfeee7a849
darrendb <darrendb@gmail.com>
2021-08-11 14:11:53 -0400

    deploy config for heroku and aws

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,30 +1,7 @@
 const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
 
 module.exports = ({ env }) => {
-
-  if (env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-            useNullAsDefault: true,
-          },
-        },
-      }
-    }
-  }
   return {
     defaultConnection: 'default',
     connections: {
