3a5aa880f57b95d941ac1a3c5d0ea46542941b1f
darrendb <darrendb@gmail.com>
2021-08-12 15:27:34 -0400

    adding npm install;

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 echo "build.sh"
+
+echo "-- running ./backend/npm install"
+echo "-- running ./frontend/npm install"
+(cd ./backend && npm install) & (cd ./frontend && npm install) &
+
 echo "-- running ./backend/npm run start"
 echo "-- running ./frontend/npm run start"
-
-(cd ./backend && npm run start) & (cd ./frontend && npm run start)
+(cd ./backend && npm run start) & (cd ./frontend && npm run start) &
 
 echo "-- done."
