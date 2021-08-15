47030747f334400d4f3fea3b7c629989fe9b1b94
darrendb <darrendb@gmail.com>
2021-08-15 14:47:30 -0400

    still seeing postgres errors; following strapi docs for postgres;

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,48 +1,49 @@
-const parse = require('pg-connection-string').parse;
 console.log("config/env/production/database.js");
+const parse = require('pg-connection-string').parse;
 // console.log(`--DATABASE_URL: ${process.env.DATABASE_URL}`);
 module.exports = ({env}) => {
   console.log(`--NODE_ENV: ${env('NODE_ENV')}`);
-  if (env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL);
-    // console.log("--config:");
-    // console.log(config);
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-            ssl: {
-              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-            }
-          },
-          options: {
-            ssl: true
-          }
-        }
-      }
-    };
-  }
+  // if (env('NODE_ENV') === 'production') {
+  const config = parse(process.env.DATABASE_URL);
+  // console.log("--config:");
+  // console.log(config);
   return {
     defaultConnection: 'default',
     connections: {
       default: {
         connector: 'bookshelf',
         settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/fc-data.db')
+          client: 'postgres',
+          host: config.host || 'localhost',
+          port: config.port || 5432,
+          database: config.database || 'strapi',
+          username: config.user || 'strapi',
+          password: config.password || 'strapi',
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)
+          }
         },
         options: {
-          useNullAsDefault: true
+          ssl: true,
+          debug: true
         }
       }
     }
-  }
+  };
+  // }
+  // return {
+  //   defaultConnection: 'default',
+  //   connections: {
+  //     default: {
+  //       connector: 'bookshelf',
+  //       settings: {
+  //         client: 'sqlite',
+  //         filename: env('DATABASE_FILENAME', '.tmp/fc-data.db')
+  //       },
+  //       options: {
+  //         useNullAsDefault: true
+  //       }
+  //     }
+  //   }
+  // }
 };
