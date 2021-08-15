b48ca62cc0edc12701f8de70f343b584d552bd7e
darrendb <darrendb@gmail.com>
2021-08-15 14:21:17 -0400

    changing database config ssl to true again;

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,12 +1,12 @@
 const parse = require('pg-connection-string').parse;
 console.log("config/env/production/database.js");
-console.log(`--DATABASE_URL: ${process.env.DATABASE_URL}`);
+// console.log(`--DATABASE_URL: ${process.env.DATABASE_URL}`);
 module.exports = ({env}) => {
   console.log(`--NODE_ENV: ${env('NODE_ENV')}`);
   if (env('NODE_ENV') === 'production') {
     const config = parse(process.env.DATABASE_URL);
-    console.log("--config:");
-    console.log(config);
+    // console.log("--config:");
+    // console.log(config);
     return {
       defaultConnection: 'default',
       connections: {
@@ -18,13 +18,13 @@ module.exports = ({env}) => {
             port: config.port,
             database: config.database,
             username: config.user,
-            password: config.password
-            // ssl: {
-            //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-            // }
+            password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            }
           },
           options: {
-            ssl: false
+            ssl: true
           }
         }
       }
